84db162459b08d26c89c45b3f751ffcf06346d9c
eskundrick <eskundrick@comcast.net>
2017-03-20 23:22:39 -0700

    Add files via upload

---
+++ Item46.py
@@ -0,0 +1,10 @@
+
+for x in range(4):
+    print(x)
+
+for x in range(3,-1,-1):
+    print(x)
+
+for x in range(8,1,-2):
+    print(x)
+
---
+++ item46IDLE.py
@@ -0,0 +1,27 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for i in range(4):
+	print(i)
+
+	
+0
+1
+2
+3
+>>> for i in range(3,-1,-1):
+	print(i)
+
+	
+3
+2
+1
+0
+>>> for i in range(8,1,-2):
+	print(i)
+
+	
+8
+6
+4
+2
+>>> 
