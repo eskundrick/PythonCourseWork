a53586df777796f521683082235c6cded5541fc4
eskundrick <eskundrick@comcast.net>
2017-03-22 15:28:19 -0700

    Add files via upload

--- item48.py
+++ item48.py
@@ -9,3 +9,15 @@ def sort_bubble(x):
 x = [67,45,2,13,1,998]
 sort_bubble(x)
 print(x)
+
+def sort_bubble(x):
+    for reorder in range(len(x)-1,0,-1):
+        for i in range(reorder):
+            if x[i]>x[i+1]:
+                temp = x[i]
+                x[i] = x[i+1]
+                x[i+1] = temp
+
+x = [89,23,33,45,10,12,45,45,45]
+sort_bubble(x)
+print(x)
